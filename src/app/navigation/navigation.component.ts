2862b2bd467a246eed99368d9155ee3385cac020
allesson <allesson@usto.re>
2023-11-24 10:26:39 -0300

    logout button

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-navigation',
@@ -18,9 +21,15 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private routerService: Router, private snackBar: MatSnackBar) {}
 
   toShow() {
     this.show = !this.show;
+  }
+  logout() {
+    this.authService.logout();
+    this.routerService.navigate(['/login']);
+    this.snackBar.open("Logout efetuado com sucesso!", 'Close', {duration: 1000});
   }
 }
